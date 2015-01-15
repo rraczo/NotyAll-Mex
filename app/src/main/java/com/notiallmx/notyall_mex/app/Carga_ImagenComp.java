252d2c2355d880638be2aa5c48f2c8989731a012
oscar <higuera86@outlook.com>
2015-01-15 14:49:57 -0600

    Imagen Zoom

--- app/src/main/java/com/notiallmx/notyall_mex/app/Carga_ImagenComp.java
+++ app/src/main/java/com/notiallmx/notyall_mex/app/Carga_ImagenComp.java
@@ -5,13 +5,25 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
+import android.graphics.Matrix;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+
+
 
 /**
  * Created by oscar on 06/06/2014.
  */
 public class Carga_ImagenComp extends Activity {
     String _IMG = "";
+    private ImageView imageView;
+    private ScaleGestureDetector scaleGestureDetector;
+    private Matrix matrix = new Matrix();
+    private float scale = 1f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +33,32 @@ public class Carga_ImagenComp extends Activity {
         setContentView(R.layout.activity_imagen_comp);
         Bundle bundle = getIntent().getExtras();//sacamos variables
         _IMG = bundle.getString("_IMG");//sacamos variables
-        ImageView imagenC = (ImageView)findViewById(R.id.imagenGrande);
+
+        imageView = (ImageView) findViewById(R.id.image_G);
+        scaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
+       // ImageView imagenC = (ImageView)findViewById(R.id.imagenGrande);
         try {
             Log.i("Que shingaos click", _IMG);
-            imagenC.setImageDrawable(procesosjsoup.dameDrawabledeURL(_IMG));
+            imageView.setImageDrawable(procesosjsoup.dameDrawabledeURL(_IMG));
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
+    public boolean onTouchEvent(MotionEvent ev) {
+        scaleGestureDetector.onTouchEvent(ev);
+        return true;
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            scale *= detector.getScaleFactor();
+            scale = Math.max(0.1f, Math.min(scale, 5.0f));
+            matrix.setScale(scale, scale);
+            imageView.setImageMatrix(matrix);
+            return true;
+        }
+    }
+
+
 }
