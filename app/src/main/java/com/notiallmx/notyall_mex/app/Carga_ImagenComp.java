80e67e94881a493b2fe327f1d0b74bd000b6c205
oscar <higuera86@outlook.com>
2015-01-16 14:37:20 -0600

    drag and zoom image

--- app/src/main/java/com/notiallmx/notyall_mex/app/Carga_ImagenComp.java
+++ app/src/main/java/com/notiallmx/notyall_mex/app/Carga_ImagenComp.java
@@ -1,8 +1,11 @@
 package com.notiallmx.notyall_mex.app;
 
 import android.app.Activity;
+import android.graphics.PointF;
 import android.os.Bundle;
+import android.util.FloatMath;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.TranslateAnimation;
@@ -12,18 +15,44 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
-
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 
 /**
  * Created by oscar on 06/06/2014.
  */
-public class Carga_ImagenComp extends Activity {
+public class Carga_ImagenComp extends Activity implements View.OnTouchListener {
+    private static final String TAG = "Touch";
+
+    // These matrices will be used to move and zoom image
+
+    Matrix savedMatrix = new Matrix();
+
+    // We can be in one of these 3 states
+    static final int NONE = 0;
+    static final int DRAG = 1;
+    static final int ZOOM = 2;
+    int mode = NONE;
+
+    // Remember some things for zooming
+    PointF start = new PointF();
+    PointF mid = new PointF();
+    float oldDist = 1f;
+
     String _IMG = "";
     private ImageView imageView;
     private ScaleGestureDetector scaleGestureDetector;
     private Matrix matrix = new Matrix();
     private float scale = 1f;
+    float[] m;
+    PointF last = new PointF();
+    float minScale = 1f;
+    float maxScale = 3f;
+    int viewWidth, viewHeight;
+    protected float origWidth, origHeight;
+
+    static final int CLICK = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,32 +62,91 @@ public class Carga_ImagenComp extends Activity {
         setContentView(R.layout.activity_imagen_comp);
         Bundle bundle = getIntent().getExtras();//sacamos variables
         _IMG = bundle.getString("_IMG");//sacamos variables
-
         imageView = (ImageView) findViewById(R.id.image_G);
-        scaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
-       // ImageView imagenC = (ImageView)findViewById(R.id.imagenGrande);
+
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 150);
+        imageView.setLayoutParams(layoutParams);
+
+
         try {
             Log.i("Que shingaos click", _IMG);
+
             imageView.setImageDrawable(procesosjsoup.dameDrawabledeURL(_IMG));
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER); // make the image fit to the center.
+            imageView.setOnTouchListener(this);
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-    public boolean onTouchEvent(MotionEvent ev) {
-        scaleGestureDetector.onTouchEvent(ev);
-        return true;
-    }
 
-    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
-        @Override
-        public boolean onScale(ScaleGestureDetector detector) {
-            scale *= detector.getScaleFactor();
-            scale = Math.max(0.1f, Math.min(scale, 5.0f));
-            matrix.setScale(scale, scale);
-            imageView.setImageMatrix(matrix);
-            return true;
+    }
+    public boolean onTouch(View v, MotionEvent event) {
+        ImageView view = (ImageView) v;
+        // make the image scalable as a matrix
+        view.setScaleType(ImageView.ScaleType.MATRIX);
+        float scale;
+
+        // Handle touch events here...
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+
+            case MotionEvent.ACTION_DOWN: //first finger down only
+                savedMatrix.set(matrix);
+                start.set(event.getX(), event.getY());
+                Log.d(TAG, "mode=DRAG" );
+                mode = DRAG;
+                break;
+            case MotionEvent.ACTION_UP: //first finger lifted
+            case MotionEvent.ACTION_POINTER_UP: //second finger lifted
+                mode = NONE;
+                Log.d(TAG, "mode=NONE" );
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN: //second finger down
+                oldDist = spacing(event); // calculates the distance between two points where user touched.
+                Log.d(TAG, "oldDist=" + oldDist);
+                // minimal distance between both the fingers
+                if (oldDist > 5f) {
+                    savedMatrix.set(matrix);
+                    midPoint(mid, event); // sets the mid-point of the straight line between two points where user touched.
+                    mode = ZOOM;
+                    Log.d(TAG, "mode=ZOOM" );
+                }
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                if (mode == DRAG)
+                { //movement of first finger
+                    matrix.set(savedMatrix);
+                    if (view.getLeft() >= -392)
+                    {
+                        matrix.postTranslate(event.getX() - start.x, event.getY() - start.y);
+                    }
+                }
+                else if (mode == ZOOM) { //pinch zooming
+                    float newDist = spacing(event);
+                    Log.d(TAG, "newDist=" + newDist);
+                    if (newDist > 5f) {
+                        matrix.set(savedMatrix);
+                        scale = newDist/oldDist; //thinking I need to play around with this value to limit it**
+                        matrix.postScale(scale, scale, mid.x, mid.y);
+                    }
+                }
+                break;
         }
+
+        // Perform the transformation
+        view.setImageMatrix(matrix);
+
+        return true; // indicate event was handled
     }
 
+    private float spacing(MotionEvent event) {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return FloatMath.sqrt(x * x + y * y);
+    }
 
+    private void midPoint(PointF point, MotionEvent event) {
+        float x = event.getX(0) + event.getX(1);
+        float y = event.getY(0) + event.getY(1);
+        point.set(x / 2, y / 2);
+    }
 }
